7b380d0d4e6e5387575c4a95200eebd179b079d3
gliegard <guillaume.liegard@ign.fr>
2021-12-15 17:34:01 +0100

    health: add health check route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/health', (req, res) => {
+  res.status(200).send('Ok');
+});
+
 module.exports = router;
